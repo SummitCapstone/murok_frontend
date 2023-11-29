8db292a1dd9cad433d40d74b04dfce73575f3cc0
zlawlghks <rlawlghks69@naver.com>
2023-11-29 22:34:43 +0900

    feat: 질병 데이터 추가

--- src/components/DetailedInfo.js
+++ src/components/DetailedInfo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { useLocation } from 'react-router-dom'; // useLocation 훅 임포트
+import diseasesData from './diseasesData';
 import './DetailedInfo.css';
 
 function DetailedInfo() {
@@ -10,30 +10,12 @@ function DetailedInfo() {
     const { cropName, sickNameKor } = location.state || {};
 
     useEffect(() => {
-        // API 호출을 위한 함수
-        const fetchNCPMSData = async () => {
-            try {
-                // Axios를 사용하여 실제 API 호출
-                const response = await axios.get('https://어쩌구/crop_api', {
-                    params: {
-                        apiKey: 'API_KEY', // 실제 API 키
-                        serviceCode: 'SVC01', // 서비스 코드
-                        serviceType: 'AA003', // 서비스 타입
-                        cropName, // 작물 이름
-                        sickNameKor // 병해 이름
-                    }
-                });
-                setInfo(response.data); // 응답 데이터로 상태 업데이트
-            } catch (error) {
-                console.error('API 호출 중 오류 발생:', error);
-                setInfo({ title: '오류', content: '정보를 불러오는 중 오류가 발생했습니다.' });
-            }
-        };
-
-        if (cropName && sickNameKor) {
-            fetchNCPMSData();
-        }
-    }, [cropName, sickNameKor]); // useEffect의 의존성 배열에 cropName과 sickNameKor 추가
+        // 데이터에서 해당 작물의 병해 정보를 찾습니다.
+        const diseaseInfo = diseasesData.find(
+          disease => disease.cropName === cropName && disease.sickNameKor === sickNameKor
+        );
+        setInfo(diseaseInfo);
+      }, [cropName, sickNameKor]);
 
     return (
         <div className="detailed-info">
---
+++ src/components/diseasesData.js
@@ -0,0 +1,98 @@
+const diseasesData = [
+    // [딸기잿빛곰팡이병]
+    {
+        "cropName": "딸기",
+        "sickNameKor": "잿빛곰팡이병",
+        "sickNameEng": "Gray mold",
+        "developmentCondition": "병원균은 병든 부위에서 균핵 혹은 분생포자의 형태로 월동하여 전염원이 된다.<br/>시설재배시 기온이 20℃내외이고 습도가 높을 때 많이 발생하며, 노지재배시는 여름철 장마기때 주로 발생한다.<br/>과실의 감염은 주로 꽃이 달린 부위에서부터 시작되므로 과실이 커지면서 꽃이 쉽사리 떨어지지 않는 품종에서 발생이 심하다.",
+        "preventionMethod": "- 병든식물체는 비닐봉지 등에 모아 매몰하거나 소각하고, 수확후에 포장관리를 철저히 한다.<br/>\n- 시설내의 온도와 습도관리를 잘 조절해 준다.<br/>\n- 꽃잎이나 병든 잔사물이 과실이나 잎에 붙지 않도록 한다.<br/>\n- 식물체가 웃자라지 않도록 투광에 유의한다.<br/>\n- 병원균은 포자형성량이 매우 높아 한번 번지면 방제하기 어려우므로, 발생초기의 집중방제가\r\n  중요하다.<br/>\n- 약제살포는 수화제나 유제보다 분제, 훈연제, 연무제 등이 습도를 높여 주지 않는 점에서 유리\r\n  하다.<br/>\n- 본 병원균은 약제내성이 잘 생기므로 계통이 다른 등록약제를 교호살포하는 것이 좋다.",
+        "symptoms": "주로 과실에 발생하나 잎, 꽃잎, 잎자루, 과병 등에도 발생한다.<br/>과실에는 작은 수침상의 담갈색 병반으로 나타타고 점차 진전되면 과실이 부패한다.<br/>부패된 과실에는 잿빛의 분생포자로 뒤덮힌다.<br/>잎에는 꽃잎이 떨어져 묻어 있는 부분에서부터 감염이 시작된다.<br/>과병과 잎자루에는 암갈색 병반이 형성되고, 진전되면 줄기가 말라죽으며, 잿빛의 곰팡이가 밀생한다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=80489538a839d37b5e03c61cd27055c437c3a93e7ffbd85f8df3c518f0029d134ae64cb816fa866ea37a1163cdf391145d18c0fed7a6833cfd64250ffcc98310",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=80489538a839d37b5e03c61cd27055c437c3a93e7ffbd85f8df3c518f0029d134ae64cb816fa866ea37a1163cdf3911402c32c9a2b851aaac9a28c4e2ec07211"
+    },
+    // [딸기흰가루병]
+    {
+        "cropName": "딸기",
+        "sickNameKor": "흰가루병",
+        "sickNameEng": "Powdery mildew",
+        "developmentCondition": "병원균은 병든 식물체의 잔재(殘滓)에 붙어 있는 균사나 분생포자의 형태로 월동하여 1차 전염원이 되는 것으로 생각된다.<br/>주로 봄과 가을의 시설재배에서 많이 발생하며, 여름에는 발생하지 않는다.",
+        "preventionMethod": "- 발병이 심한지역은 저항성품종을 재배한다(수동, 보교조생, 히미꼬 등).\r\n  - 합리적 시비로 왕성한 생육을 유도한다.<br/>\n  - 병든 잔사물은 일찍 제거하여 전염원을 없애고, 수확후의 포장관리에 유의한다.<br/>  \r\n  - 발생초기에 예방적 약제살포로 2차 전염원의 밀도를 줄여준다.<br/>\n  - 다양한 약제가 등록되어 있다.",
+        "symptoms": "잎, 과실, 과병에 발생한다.<br/>잎에서는 잎 뒷면에 흰색의 균총으로 나타나고, 과실에는 흰가루를 뿌려 놓은 것 같은 증상으로 나타난다.<br/>어린 과실은 비대가 억제되고 경화되며, 착과기의 과실에 발생하면 상품가치가 떨어진다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=ca0528fa179d091b72aa8780a6447db18bbab82608d72e09da0bd0c0a931c7c4e93ea25df751c1aa452075a1f8468e891c884d81eeed2937e87bb95a433959a55d18c0fed7a6833cfd64250ffcc98310",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=ca0528fa179d091b72aa8780a6447db18bbab82608d72e09da0bd0c0a931c7c4e93ea25df751c1aa452075a1f8468e891c884d81eeed2937e87bb95a433959a502c32c9a2b851aaac9a28c4e2ec07211"
+    },
+    // [오이노균병]
+    {
+        "cropName": "오이",
+        "sickNameKor": "노균병",
+        "sickNameEng": "Downy mildew",
+        "developmentCondition": "노균병은 거의 모든 박과 채소작물에 발생하지만 특히 오이에 피해가 큰 중요 병해로서 보통 생육 중기 이후부터 잎에 발생한다.<br/>생육 후기에 저온, 다습하면 아랫잎부터 발생하여 큰 피해를 주는데, 최소 6시간 동안 100%의 상대습도가 유지되어야만 병원균의 포자낭이 형성되고, 발병 온도 범위는 5～30℃이며, 발병 적온은 15～20℃이다.<br/>병원균은 병든 식물체의 조직속에서 난포자 상태로 겨울을 지내고 새로운 식물체가 파종되어 생장하면 다시 발아하여 지상부로 침입하고, 잎 뒷면에서 다량의 포자를 형성하여 공기 중으로 쉽게 전반된다.<br/>병원균의 균사가 잎의 기공이나 수공으로 침입하여 세포간극에서 생장하며 흡기를 내어 세포의 영양을 흡수한다.",
+        "preventionMethod": "- 환기를 철저히 하고 하우스내 토양이 과습하지 않도록 한다.<br/>\r\n- 병든 잎은 조기에 제거하여 불에 태우거나 땅속 깊이 묻는다.<br/>\n- 등록약제를 이용하여 방제한다.",
+        "symptoms": "초기에는 잎의 앞면에 녹색이 퇴색된 부정형 반점이 생기고 엷은 황색을 띄는데, 잎 뒷면에는 불분명하게 보인다.<br/>아랫잎에서 먼저 발생되고 위로 번지며, 반점들이 합쳐지면 병반은 커지고 잎이 말라죽는다.<br/>이때 병든 잎은 잘 찢어지고, 황갈색을 띤다.<br/>잎 뒷면에는 이슬처럼 보이는 곰팡이가 다량 형성되어 흰색 혹은 회색으로 보인다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=1afbddca007844ff529fb1b92402e6e26262f7a856fa040cce079db4328cda3b67e51c7a923c7d76f0deb200fe7ee132",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=1afbddca007844ff529fb1b92402e6e26262f7a856fa040cce079db4328cda3b459baf4676f2035ea8505226ea2c8e11"
+    },
+    // [오이흰가루병]
+    {
+        "cropName": "오이",
+        "sickNameKor": "흰가루병",
+        "sickNameEng": "Powdery mildew",
+        "developmentCondition": "자낭각의 형태로 병든 식물체의 잔재에서 겨울을 지내고 1차전염원이 되며, 시설재배에서는 분생포자가 공기전염 되어 계속해서 발생한다.<br/>본 병은 일반적으로 15～28℃에서 많이 발생되며, 32℃이상의 고온에서는 병 발생이 억제된다.<br/>노지포장에서는 억제재배 오이에서 심하게 발생한다.<br/>특히 일조가 부족하고, 밤낮의 온도차가 심하며, 다비재배를 할 때 병 발생이 많아진다.",
+        "preventionMethod": "- 수확 후 병든 잔재물을 제거하여 태운다.<br/>\n- 밀식을 피하고 통풍이 잘되게 하여준다.<br/>\n- 질소질 비료의 과용을 피한다.<br/>\n- 등록약제를 이용하여 방제한다.",
+        "symptoms": "주로 잎에 발생한다.<br/>처음에는 잎의 표면에 소량의 흰가루가 밀생하며, 진전되면 잎 전체가 흰가루로 뒤덮힌다.<br/>오래된 병반상에서는 흰가루가 회백색으로 변하고, 흑색의 소립점(자낭각)이 형성된다.<br/>후에 병든 잎은 고사한다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=d3e24720a558b196a99707a4d31866b60ec6daca4b7c2e45b05e1f7c59c95b8d000100ae70caf9e822dca55bc23fb0526d049319a462d87d12fcb28d9440e618",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=d3e24720a558b196a99707a4d31866b60ec6daca4b7c2e45b05e1f7c59c95b8d3f6c8f152ce6aee544159c91370cb596"
+    },
+    // [토마토흰가루병]
+    {
+        "cropName": "토마토",
+        "sickNameKor": "흰가루병",
+        "sickNameEng": "Powdery mildew",
+        "developmentCondition": "노지재배에서는 피해가 매우 적으나 시설재배에서 많이 발생한다.<br/>온도가 서늘한 봄과 가을에 많이 발생하며, 다습한 조건보다는 건조한 환경하에서 피해가 크다.<br/>분생포자 또는 균사의 형태로 병든 식물체의 잔재에서 겨울을 지내고 1차 전염원이 되며, 그 후의 발병은 분생포자가 공기전염되어 계속적으로 발생한다.<br/>발병 적온은 25～28℃이다.",
+        "preventionMethod": "- 병든 잎은 일찍 제거 소각 또는 묻는다.<br/>\n- 밀식을 피하고 잎이 너무 무성하지 않도록 관리한다.<br/>\n- 하우스 내 환기 시 너무 건조하지 않도록 한다.<br/>\n- 병원균의 밀도 증가가 빠르므로 발병 초기에 관리를 잘 해야 한다.",
+        "symptoms": "주로 잎에 발생하며, 심하면 잎자루나 과경에도 발생한다.<br/>잎에서는 처음 흰가루가 불규칙한 모양으로 점점이 나타나고, 진전되면 잎 전체가 흰가루로 뒤 덮힌다.<br/>심하게 발생하면 잎의 피해부가 누렇게 되고 말라 죽는다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=eadf17264facd8047fd3c40fe8ff02bd01b3e014aec2bc18d66abaad8a1fb9c2e88a1532f142c509d591ebd71728a700b7eac3069163423ca8f33135406e754f",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=eadf17264facd8047fd3c40fe8ff02bd01b3e014aec2bc18d66abaad8a1fb9c2e88a1532f142c509d591ebd71728a700b7b53e57f59596a7aeb0963ec363176c"
+    },
+    // [토마토잿빛곰팡이병]
+    {
+        "cropName": "토마토",
+        "sickNameKor": "잿빛곰팡이병",
+        "sickNameEng": "Gray mold",
+        "developmentCondition": "병원균은 균핵이나 분생포자의 형태로 토양이나 병든 식물체의 잔재에서 겨울을 지내고 1차 전염원이 되며, 2차 전염은 기주작물의 병반 상에 형성된 분생포자가 비산하여 이루어진다.<br/>20℃ 전,후의 저온과 다습 조건이 가장 중요한 다 발생 원인이며, 특히 시설재배지에서 피해가 크다.",
+        "preventionMethod": "- 하우스내 온,습도의 관리를 철저히 조절한다.<br/>\n- 병든 잔사물은 비닐봉지에 넣어 하우스 밖에서 불태우거나 또는 땅에 묻는다.<br/>\n- 꽃잎이 과일이나 잎, 줄기에 묻어 있지 않도록 한다.<br/>\n- 식물체가 너무 도장하지 않도록 시비 조절과 투광에 유의한다.<br/>\n  - 병원균의 밀도증식이 매우 빨라 한번 번지면 방제하기 어려우므로 발생 초기에 집중적으로 방제를 한다.<br/>\n  - 약제사용은 수화제보다 분제 또는 훈연제, 연무제등을 사용하는 것이 효과적이다.<br/>\n  - 본 병원균은 액제내성이 매우 잘 생기므로 약제를 살포할때는 계통이 다른 등록약제를 번갈아 살포 해야한다.",
+        "symptoms": "과실, 잎, 과경, 잎자루 등 지상부 모든 부위에 발생하나 특히 잎과 과실에서의 발생이 심하다.<br/>잎의 끝부분부터 병반이 시작되는 일이 많으며, 초기에는 갈색의 소형 반점으로 나타나고, 진전되면 대형 병반으로 확대된다.<br/>심하면 잎 전체가 말라 죽는다.<br/>과실에는 처음 암갈색의 작은 원형 병반으로 나타나고, 진전되면 과실 전체가 물러 썩는다.<br/>줄기는 꽃잎이나 잎이 떨어져 붙어 있는 부위에서 발병이 시작되는 일이 많은데, 처음에는 갈색의 작은 반점으로 시작되어 심해지면 줄기 전체가 갈색으로 변하여 말라 죽는다.<br/> 병든 부위에는 잿빛의 분생포자가 밀생하는 것이 특징이다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=6340fa0bf55dc4d88aa23f3605e513f661eca807684c624ef23526e2441b37efde593743c2ad5b358b2d6834dda0d582d4ca8dbad53cb4b43f8d99ea274736e5",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=6340fa0bf55dc4d88aa23f3605e513f661eca807684c624ef23526e2441b37efde593743c2ad5b358b2d6834dda0d582e90bde49edc7f81524f1cd58f094f3c9"
+    },
+    // [고추탄저병]
+    {
+        "cropName": "고추",
+        "sickNameKor": "탄저병",
+        "sickNameEng": "Anthracnose",
+        "developmentCondition": "병원균은 종자 혹은 병든 부위에서 자낭각과 균사의 형태로 겨울을 지내고 1차 전염원이 된다.<br/>병의 전반은 주로 분생포자에 의해 이루어지며, 시설재배 포장보다는 노지포장에서 병 발생이 심하다.<br/>노지포장에서는 여름철 장마기에 분생포자가 주로 비, 바람에 의해 전반된다.<br/>노지재배의 풋고추에서는 7월 초순부터 병이 발생하기 시작하여 수확기까지 계속 발생한다.",
+        "preventionMethod": "- 건전종자를 파종하고, 건전묘를 이식한다.<br/>\n  - 종자를 소독하여 파종한다.<br/>\n  - 이 병에 잘 걸리지 않는 품종을 선택하여 재배한다.<br/>\n  - 등록약제를 병 발생 초기부터 살포한다.",
+        "symptoms": "주로 과실에 발생한다.<br/>과실에는 처음에 감염부위가 수침상으로 약간 움푹 들어간 원형반점으로 나타나고, 진전되면 병반이 원형 내지 부정형의 겹무늬 증상으로 확대된다.<br/>병반부위에는 담황색 내지 황갈색의 포자덩어리가 형성되고, 심하게 병든 과실은 비틀어지고 미이라처럼 말라버린다.<br/>성숙과의 병반은 간혹 흑색의 겹무늬 증상을 띄는 것도 있으며, 수확 후 건조하는 과정에서 병 증상이 나타나는 것도 있다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=3dca1f0c424be9a24698ece85084f1b1a524125a81d33fc719377a3505ca1dd05b610a0af329f61519b148fdc97d8f4f547119ebf2072ece317553416553813b24a8419889bf650128bb2b4acfc38c2e",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=3dca1f0c424be9a24698ece85084f1b1a524125a81d33fc719377a3505ca1dd05b610a0af329f61519b148fdc97d8f4fc40a3b0d2752d85777a86653715f5df6fe234dbb9b0b97e48b03b12ae0114e5d"
+    },
+    // [고추흰가루병]
+    {
+        "cropName": "고추",
+        "sickNameKor": "흰가루병",
+        "sickNameEng": "Powdery mildew",
+        "developmentCondition": "고추와 오크라,  토마토, 가지, 오이 등이 병 발생 기주로 알려져 있다.<br/>시설재배지와 가을 날씨가 건조할 때 심하게 발생한다.<br/>분생포자는 공기전염되며, 건조한 조건하에서는 80일간 전염원 능력이 유지된다.<br/>흰가루병균은 하우스 내에서도 난방기가 설치된 곳이나 남쪽 출입구 부근에서 많이 발생하는 것을 볼 수 있다.",
+        "preventionMethod": "- 병든 잎은 일찍 제거하여 초기 전염원을 없앤다.<br/>\n  - 질소비료의 지나친 사용을 피하고 과번무 하지 않게 관리한다.<br/>\n  - 다소 건조하고 서늘한 조건에서 많이 발생하므로, 다른 병의 발생을 조장하지 않는한 온도와\r\n    습도를 높여준다.<br/>\n  - 내부기생성 병원균이므로 침투이행성 등록약제를 살포한다.<br/>\n  - 발병 초 예방적으로 등록약제를 살포하는 것이 효과적이다.",
+        "symptoms": "주로 잎에 발병하고 발병 초기에는 잎 뒷면의 한 부분이 흰 가루가 묻은 것처럼 보이는데, 이것은 흰가루병균이 잎 표면에 정착한 후 만들어낸 분생포자이다.<br/>병이 진전되면 잎 뒷면은 지저분하게 흰 가루가 퍼지고 오래된 병반 주위에 흑색소립으로 보이는 자낭각이 형성된다.<br/>잎의 앞뒷면에 발생하며 뒷면에 병이 더 심하게 발생한다.<br/>병 발생이 지속되면 잎은 점차 누렇게 변하고 얼룩덜룩해지면서 아래 잎부터 말라 떨어진다.",
+        "infectionRoute": null,
+        "thumbImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=e8747a8695d576f25ba603e83dbd775eeaebe819bfed7a1f6d6c115444382f0c23d8583d7cd665011471fcbb0db6fd9e134bb6a38e53516476e4faf1249ec91b24a8419889bf650128bb2b4acfc38c2e",
+        "oriImg": "http://ncpms.rda.go.kr/npmsAPI/thumbnailViewer2.mo?uploadSpec=npms&uploadSubDirectory=/photo/sickns/&imageFileName=e8747a8695d576f25ba603e83dbd775eeaebe819bfed7a1f6d6c115444382f0c23d8583d7cd665011471fcbb0db6fd9e40f049fe95cef94753c874168c2b4b93fe234dbb9b0b97e48b03b12ae0114e5d"
+    }
+];
