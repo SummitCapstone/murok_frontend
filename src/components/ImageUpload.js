ce36e5086c81bad68b4d5ab70e9e6c429bec6d2d
zlawlghks <rlawlghks69@naver.com>
2023-12-07 03:59:15 +0900

    feat: 이미지 파일 형식 에러 핸들링 추가

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -5,13 +5,21 @@ function ImageUpload({ setSelectedImage }) {
   const [previewUrl, setPreviewUrl] = useState(null);
   const [fileName, setFileName] = useState('');
   const [imageSize, setImageSize] = useState(100); // 이미지 크기 상태
+  const [errorMessage, setErrorMessage] = useState(''); // 오류 메시지 상태 추가
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
+      // 지원되는 이미지 형식인지 확인
+      if (!file.type.match('image/jpeg') && !file.type.match('image/png')) {
+        setErrorMessage('지원하지 않는 형식의 파일입니다.\njpg, jpeg, png 형식만 지원합니다.');
+        return; // 지원되지 않는 형식일 경우 함수 종료
+      }
+
       setSelectedImage(file);
       setFileName(file.name); // 파일 이름 저장
       setPreviewUrl(URL.createObjectURL(file)); // 미리보기 URL 생성
+      setErrorMessage(''); // 오류 메시지 초기화
     }
   };
 
@@ -34,6 +42,7 @@ function ImageUpload({ setSelectedImage }) {
         )}
         <input type="file" accept="image/*" hidden onChange={handleImageChange} />
       </label>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
       {fileName && <div className="file-name">{fileName}</div>}
       {previewUrl && (
         <div className="image-size-slider">
